7def36d3da28a95d310d9addaa1510b94621dc69
Luis José Sánchez <luisjoseprofe@gmail.com>
2024-12-09 16:45:34 +0100

    Hola mundo en Streamlit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("¡Hola mundo!")
+
+st.write("Esta es una aplicación de prueba de Streamlit.")
+
+# # Add a text input
+# name = st.text_input("Enter your name:")
+
+# # Display the entered name
+# if name:
+#     st.write("Hello, {0}!".format(name))
+
